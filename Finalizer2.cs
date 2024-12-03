e37492c7b98caa815a9ee42d430a53b9b762ce18
zeusvikey <127013256+zeusvikey@users.noreply.github.com>
2024-12-03 22:42:27 +0530

    Add files via upload
    i have make a basic programe to understand the work of finalizer .if you want run the code then open visual studio only for c# then create a project called finalizer and name of the class must bi Finalizer2 just like this =class Finalizer2{}.Then run its will be work

---
+++ Finalizer2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace oopsProject1
+{
+    internal class Finalizer2
+    {
+        string name;
+
+       
+        public Finalizer2()
+        {
+            name = "Default Name"; 
+            Console.WriteLine("Constructor called, Name: " + name);
+        }
+
+       
+        ~Finalizer2()
+        {
+            Console.WriteLine("Destructor called");
+        }
+
+        static void Main(string[] args)
+        {
+            Finalizer2 obj = new Finalizer2();
+            obj.name = "Ed Sheeran";
+            Console.WriteLine("Updated Name: " + obj.name);
+
+           
+            Console.ReadLine();
+        }
+    }
+}
